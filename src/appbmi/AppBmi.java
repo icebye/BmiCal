c55ac7d5995ec80967040241d55ed139a7b0ec38
icebye <icebye@LAPTOP-ASNN259A>
2023-04-01 16:04:52 +0700

    First Comment

---
+++ src/appbmi/AppBmi.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appbmi;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author icebye
+ */
+public class AppBmi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AppUi ui = new AppUi();
+        ui.setLocationRelativeTo(null);
+        ui.setVisible(true);
+    }
+    
+}
